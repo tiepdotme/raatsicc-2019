dff8c2bf83bf1548039a58856ba7f8351b2534f4
Callum Flack <callumflack@gmail.com>
2019-05-07 19:30:38 +1000

    create: Button comp

---
+++ components/Button.vue
@@ -0,0 +1,32 @@
+<template>
+  <component :is="type" :href="href" class="button">
+    <slot />
+  </component>
+</template>
+
+<script>
+// https://thoughts.codegram.com/create-a-reusable-button-with-vue-dynamic-components/
+export default {
+  props: {
+    href: {
+      type: String,
+      default: null
+    },
+    to: {
+      type: String,
+      default: null
+    }
+  },
+  computed: {
+    type() {
+      if (this.to) {
+        return "nuxt-link";
+      } else if (this.href) {
+        return "a";
+      } else {
+        return "button";
+      }
+    }
+  }
+};
+</script>
