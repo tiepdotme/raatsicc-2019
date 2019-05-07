251f4e9fc1fd2c9878f8a76d83866c3132e0a7a5
Callum Flack <callumflack@gmail.com>
2019-05-07 19:30:56 +1000

    create: comps for data

---
+++ components/CardTeamMember.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="Card">
+    <figure v-if="image" class="Card-image mt-0">
+      <img :src="image.url" :alt="name" />
+    </figure>
+    <p class="text-sm font-bold mb-0">{{ name }}</p>
+    <p class="text-sm mb-0">{{ title }}</p>
+    <p class="text-sm text-gray-600 mb-0">{{ location }}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["image", "name", "title", "location"]
+};
+</script>
+
+<style scoped>
+.Card-image {
+  margin-bottom: var(--s-figure) !important;
+}
+</style>
---
+++ components/PostExcerpt.vue
@@ -0,0 +1,50 @@
+<template>
+  <nuxt-link class="Post" :to="`/news/${slug}`">
+    <h3 class="Heading mb-2">{{ title }}</h3>
+    <div class="Meta mb-4">
+      <time>{{ date }}</time>
+      <span class="mx-1">|</span>
+      <cite class="not-italic">{{ author }}</cite>
+      <template v-for="(tag, index) in tags">
+        <span :key="index" class="Post-tag">{{ tag }}</span>
+      </template>
+    </div>
+    <figure v-if="image" class="Post-image">
+      <img :src="image.url" alt="title" />
+    </figure>
+    <p>
+      {{ excerpt }}
+      <span>Continue reading</span>
+    </p>
+  </nuxt-link>
+</template>
+
+<script>
+export default {
+  props: ["slug", "title", "tags", "date", "author", "image", "excerpt"]
+};
+</script>
+
+<style scoped>
+.Post {
+  @apply block;
+}
+
+.Post + .Post {
+  @apply pt-12;
+}
+
+.Post-tag {
+  @apply text-gray-300 bg-brand-green ml-2 px-2 py-1 rounded-sm;
+}
+
+.Post-image {
+  @apply bg-brand-neutral;
+  height: 25vh;
+  margin-bottom: var(--s-figure) !important;
+}
+
+.Post-image img {
+  @apply h-full mx-auto;
+}
+</style>
