430008674e71cef41126dbba6b21c4fb5d9a1445
Callum Flack <callumflack@gmail.com>
2019-05-07 10:56:57 +1000

    update: better linting setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,18 +4,32 @@ module.exports = {
     browser: true,
     node: true
   },
+  globals: {
+    $nuxt: true
+  },
   parserOptions: {
     parser: "babel-eslint"
+    // parser: "vue-eslint-parser"
   },
-  extends: ["eslint:recommended", "plugin:vue/recommended"],
+  extends: [
+    "plugin:vue/recommended",
+    "eslint:recommended",
+    "prettier/vue",
+    "plugin:prettier/recommended"
+  ],
   // required to lint *.vue files
   plugins: ["vue"],
   // add your custom rules here
   rules: {
     "vue/no-unused-vars": ["warn"],
     "vue/require-prop-types": 0,
+    "vue/require-default-prop": 0,
+    "vue/no-v-html": 0,
     "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
     "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+    // conflicts with prettier
+    "vue/singleline-html-element-content-newline": "off",
+    "vue/html-self-closing": "off",
     // https://github.com/prettier/eslint-plugin-prettier/issues/94
     "vue/max-attributes-per-line": [
       2,
