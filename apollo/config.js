658e7964664d0e0a1441b79075d139f44d55d46e
Callum Flack <callumflack@gmail.com>
2019-05-09 17:37:02 +1000

    create: nice apollo gql store for allLocations

--- apollo/config.js
+++ apollo/config.js
@@ -2,5 +2,6 @@ export default function() {
   return {
     httpEndpoint: "https://graphql.datocms.com",
     getAuth: () => "55524c93dac61709c12e1514c04412" // Bearer added by default
+    // getAuth: () => "PROCESS.ENV.DATO_TOKEN" // Bearer added by default
   };
 }
