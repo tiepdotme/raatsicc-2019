28fd80652b0c3bb60a98d0aa95327c11d9105531
Callum Flack <callumflack@gmail.com>
2019-06-05 16:28:05 +1000

    update: better dato naming (WhatSubpages not WhatPages)

--- pages/what-we-do/index.vue
+++ pages/what-we-do/index.vue
@@ -4,15 +4,14 @@
 
     <ContentColumn text>
       <h3 class="Heading italic">
-        The RAATSICC philosophy is that no-one knows our children and families
-        in our remote communities better than the families themselves.
+        {{ whatPage.subtitle }}
       </h3>
     </ContentColumn>
     <ContentColumn>
-      <div class="Markdown">
-        <p>We have four pages here…</p>
-        <ul v-if="allWhatPages">
-          <li v-for="item in allWhatPages" :key="item.title">
+      <div class="Markdown" v-html="$md.render(whatPage.lede)" />
+      <div class="Markdown st-paragraph">
+        <ul v-if="allWhatSubpages">
+          <li v-for="item in allWhatSubpages" :key="item.title">
             <nuxt-link :to="`/what-we-do/${item.slug}`">
               {{ item.title }}
             </nuxt-link>
@@ -34,17 +33,22 @@ export default {
     ContentColumn
   },
   apollo: {
-    allWhatPages: gql`
+    allWhatSubpages: gql`
       {
-        allWhatPages(orderBy: [order_ASC]) {
+        allWhatSubpages {
           slug
           title
-          order
+        }
+      }
+    `,
+    whatPage: gql`
+      {
+        whatPage {
+          subtitle
+          lede
         }
       }
     `
   }
 };
 </script>
-
-<style></style>
