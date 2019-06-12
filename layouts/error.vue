b166b0817b00e529b08caa903120a9b1ad7188df
Callum Flack <callumflack@gmail.com>
2019-06-12 15:59:42 +1000

    update: style 404 pg

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,17 +1,65 @@
-<template lang="pug">
-div
-  section.b-ptTitle
-    .Container.Header--animate
-      h1.Supertitle(v-if="error.statusCode === 404") Unfortunately, I've probably removed that page or stuffed around with it's location. Sorry.
-      h1.Supertitle(v-else) An error occurred
-      p
-        nuxt-link(to="/") Start again at the home page →
+<template>
+  <div class="__nuxt-error-page">
+    <div class="Section Section-b error">
+      <icon class="text-black" name="info" width="90" height="90" />
+
+      <div v-if="statusCode === 404" class="description">
+        <h1 class="Title st-h s-2xh">Page not found - 404</h1>
+        <NuxtLink class="Heading error-link" to="/">
+          <icon name="arrow-left" />
+          Go Back Home
+        </NuxtLink>
+      </div>
+      <div v-else>
+        <div class="Title st-h s-2xh">{{ message }}</div>
+        <p v-if="debug" class="description">
+          {{ messages.client_error_details }}
+        </p>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-// See also: https://github.com/nuxt/nuxt.js/blob/dev/packages/app/template/components/nuxt-error.vue
 export default {
-  props: ["error"],
-  layout: "default" // you can set a custom layout for the error page
+  name: "NuxtError",
+  layout: "default",
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  head() {
+    return {
+      title: this.message,
+      meta: [
+        {
+          name: "viewport",
+          content:
+            "width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no"
+        }
+      ]
+    };
+  },
+  computed: {
+    statusCode() {
+      return (this.error && this.error.statusCode) || 500;
+    },
+    message() {
+      return this.error.message || this.messages.client_error;
+    }
+  }
 };
 </script>
+
+<style>
+.error {
+  /* max-width: 600px;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column; */
+}
+</style>
