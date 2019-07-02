eabd5c7ff6266886c869bdf3fe9f1e10c4dd0799
Callum Flack <callumflack@gmail.com>
2019-07-02 14:16:46 +1000

    Update ImageDynamic.vue

--- components/ImageDynamic.vue
+++ components/ImageDynamic.vue
@@ -1,23 +1,9 @@
 <template>
-  <no-ssr>
-    <figure>
-      <!-- SVG blur -->
-      <svg xmlns="http://www.w3.org/2000/svg" version="1.1" class="hidden">
-        <defs>
-          <filter id="blur">
-            <feGaussianBlur :stdDeviation="deviation" in="SourceGraphic" />
-          </filter>
-        </defs>
-      </svg>
-      <!-- v-lazy-image -->
-      <v-lazy-image
-        :src="url"
-        :src-placeholder="urlPlaceholder"
-        :srcset="srcset"
-        :alt="alt"
-      />
-    </figure>
-  </no-ssr>
+  <figure ref="image" :class="{ cover: cover }">
+    <no-ssr>
+      <v-lazy-image :src="url" :src-placeholder="urlPlaceholder" />
+    </no-ssr>
+  </figure>
 </template>
 
 <script>
@@ -29,94 +15,80 @@
  *
  * Component features:
  * - Optimize quality for speed
- * - Responsive image sizes using srcset
+ * - Responsively sizes images to size of the container element
  * - Lazy loads offscreen image
- * - SVG for a better, frostier blur than CSS
  */
-
-// const optimizeParams = "fit=crop&crop=edges&auto=format,enhance&q=50";
 const optimizeParams = "auto=format&q=33";
-const sizes = [480, 640, 960, 1200];
 
 export default {
   props: {
     src: String,
     alt: String,
-    maxWidth: Number,
-    blurLevel: {
-      type: Number,
-      default: 30
+    cover: {
+      type: Boolean,
+      default: false
     },
-    duration: {
+    maxWidth: {
       type: Number,
-      default: 1000
+      default: 2000
     }
   },
   data() {
     return {
       url: null,
-      urlPlaceholder: null,
-      srcset: null,
-      rate: 1
+      urlPlaceholder: null
     };
   },
-  computed: {
-    deviation() {
-      return this.blurLevel * this.rate;
-    }
-  },
-  created() {
-    const maxWidth = this.maxWidth || sizes[sizes.length - 1];
+  mounted() {
+    // Placeholder
     const url = `${this.src}?${optimizeParams}`;
-    this.urlPlaceholder = `${url}&blur=100&w=50`;
-    this.url = `${url}&max-w=${maxWidth}`;
-    this.srcset = sizes
-      .filter(size => size <= maxWidth)
-      .map(size => `${url}&w=${size * 1.5} ${size}w`)
-      .join(", ");
-  },
-  methods: {
-    animate() {
-      const start = Date.now() + this.duration;
+    this.urlPlaceholder = `${url}&blur=20&w=50`;
 
-      const step = () => {
-        const remaining = start - Date.now();
-
-        if (remaining < 0) {
-          this.rate = 0;
-        } else {
-          this.rate = remaining / this.duration;
-          requestAnimationFrame(step);
-        }
-      };
-
-      requestAnimationFrame(step);
-    }
+    // Full size
+    const scaledWidth = this.$refs.image.clientWidth * 2;
+    const width = Math.min(scaledWidth, this.maxWidth);
+    this.url = `${url}&w=${width}`;
   }
 };
 </script>
 
-<style scoped>
+<style lang="postcss" scoped>
 /*
-  1. enable Safari to keep sharp edges
-     …but only works if pos-abs within an aspect-ratio div.
+  1. enable Safari to keep sharp edges (if parent is pos-abs)
 */
 .v-lazy-image {
   min-height: 100%;
   min-width: 100%;
-  filter: url("#blur");
-  transition: filter 1s;
+  filter: blur(10px);
+  transition: filter 0.5s ease-in;
   transform: scale(1); /*1 */
 }
 .v-lazy-image-loaded {
-  filter: none;
+  filter: blur(0);
 }
 
+/*
+  Flash of layout before any images render.
+ */
 figure {
-  @apply m-0;
+  @apply bg-gray-200;
 }
 
+/*
+  Figure parent containing a dynamic image that provides
+  an exact area to cover when the image doesn't use an aspect ratio.
+  The parent must be relative.
+ */
+figure.cover {
+  @apply absolute inset-0;
+}
+
+/*
+  Dynamic image within an figure.
+  …for flexible use, no absolute positoning is applied.
+  1. cover the figure parent area
+*/
 img {
-  @apply object-cover;
+  @apply object-cover h-full w-full; /* 1 */
 }
 </style>
